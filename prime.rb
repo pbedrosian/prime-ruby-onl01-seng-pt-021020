7eef7184478f13daca7cace2fdbd8c4ee0b438bc
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-20 00:09:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,11 @@
-require 'prime'
-
-prime.each(200000) do |prime|
-  p prime
+def prime?(n)
+    if n <= 1
+      return false
+    else
+        if (2...n).any? { |i| n % i  == 0}
+            return false
+        else
+            return true
+        end
+    end
 end
